f4b804a6e153a6aca69a93be733ab068d36609d6
Baochuan Lu <blu@sbuniv.edu>
2017-12-04 15:59:42 +0000

    removed AStar implementation

--- maze/AStar.java
+++ maze/AStar.java
@@ -1,29 +1,4 @@
 package maze;
 
 public class AStar<T> implements Agenda<T>{
-  private Heap<T> h;
-
-  public AStar(){
-    h = new Heap<T>();
-  }
-
-  public boolean isEmpty(){
-    return h.isEmpty();
-  }
-
-  public int size(){
-    return h.size();
-  }
-
-  public void add(T newItem){
-    h.insert(newItem);
-  }
-
-  public T remove(){
-    return h.remove();
-  }
-
-  public T peek(){
-    return h.peek();
-  }
 }
